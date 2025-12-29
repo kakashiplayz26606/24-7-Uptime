6f4330fa11957df5777a86e6cd6420c6432c8513
kakashiplayz26606 <ezuieoeiej@gmail.com>
2025-12-29 22:42:52 +0530

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import time
+import argparse
 import uvicorn
 
 app = FastAPI()
@@ -13,9 +14,13 @@ def ping():
     return {"pong": True, "time": time.time()}
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8080)
+    args = parser.parse_args()
+
     uvicorn.run(
         "connector:app",
         host="0.0.0.0",
-        port=8080,
+        port=args.port,
         log_level="error"
     )
